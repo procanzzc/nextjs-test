834946509489413f464e78fe9f3b696862bd8aa0
Will <jimmy@openleverage.finance>
2024-11-04 16:59:11 +0800

    add network component

---
+++ app/components/networkStatus.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from 'react';
+
+const NetworkStatus = () => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+
+    setIsOnline(navigator.onLine);
+
+
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  if (!isOnline) {
+    return (
+      <div className="fixed top-0 left-0 bg-amber-100 w-full">
+        No Network
+      </div>
+    );
+  }
+
+  return null;
+};
+
+
+
+export default NetworkStatus;
